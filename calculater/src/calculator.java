e90b41e007f34b16417a7d7c4e3cd9364f9b62aa
Baseerahmad <naragundbashir@gmail.com>
2022-01-28 14:48:08 +0530

    new

--- calculater/src/calculator.java
+++ calculater/src/calculator.java
@@ -8,7 +8,7 @@ public class calculator {
 		Scanner r=new Scanner(System.in);
 		n1=r.nextInt();
 		n2=r.nextInt();
-		System.out.println("Select Opereation ");
+		System.out.println("Select Opereation 1)add 2)sub 3)Mul 4)div 5)Rem ");
 		ch=r.nextInt();
 		
 		if(ch==1)
@@ -19,22 +19,22 @@ public class calculator {
 		else if (ch==2)
 		{
 			cal=n1-n2;
-			System.out.print("Subtrasction"+cal);
+			System.out.print("Subtrasction: "+cal);
 		}
 		else if (ch==3)
 		{
 			cal=n1*n2;
-			System.out.print("Multiplication"+cal);
+			System.out.print("Multiplication: "+cal);
 		}
 		else if (ch==4)
 		{
 		cal=n1/n2;
-		System.out.print("Division"+cal);
+		System.out.print("Division: "+cal);
 		}
 		else
 		{
 			cal=n1%n2;
-			System.out.print("Remainder"+cal);
+			System.out.print("Remainder:"+cal);
 		}
 		
 		}
