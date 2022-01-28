3ba27f13dbaadb122dfa6b52877fd6be4ad98ba5
Baseerahmad <naragundbashir@gmail.com>
2022-01-28 14:24:19 +0530

    div5

---
+++ div5.java
@@ -0,0 +1,19 @@
+package simjava;
+import java.util.Scanner;
+public class div5 {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter any number:");
+		n=s.nextInt();
+		if(n / 5 == 0) {
+			System.out.println(n+"is divisible by 5");
+		}
+			else 
+			{
+				System.out.println(n+"is not divisible by 5");
+			
+			}
+	}
+}
