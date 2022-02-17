5ea5c3e7522dc2e10d2c480ee0ada1e3b14697d9
Baseerahmad <naragundbashir@gmail.com>
2022-02-17 18:57:36 +0530

    main project pahse-1

---
+++ phase1Project/src/phase1Project/LockedMeMain.java
@@ -0,0 +1,18 @@
+package phase1Project;
+
+public class LockedMeMain {
+	public static void main(String[] args) {
+		
+		// Create "LockedMe_main" folder if not present in current folder structure
+	
+		businessLvelOperation.create_MainFolder_IfNot_Present("LockedMe_main");
+			
+		// Print the welcome screen in the console
+			
+			MenuOptions.welcome_Screen("LockedMe", "Baseerahamd Naragund ");
+			
+		// Print the main menu for the operation
+			Menu_Operation_Handling.welcome_Screen_Inputs();
+	}
+}
+ 
